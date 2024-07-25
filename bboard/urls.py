18f060f12c242fe30403dbdd6e97056710034b2e
svoyo nazvanie <171016036@step.kz>
2024-07-25 21:34:29 +0500

    dz

--- bboard/urls.py
+++ bboard/urls.py
@@ -10,9 +10,4 @@ urlpatterns = [
     path('add/', BbCreateView.as_view(), name='add'),
     path('<int:rubric_id>/', by_rubric, name='by_rubric'),
     path('', index, name='index'),
-
-
-    # re_path(r'^add/$', BbCreateView.as_view(), name='add'),
-    # re_path(r'^(?P<rubric_id>[0-9]*)/$', by_rubric, name='by_rubric'),
-    # re_path(r'^$', index, name='index'),
 ]
